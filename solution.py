c9540dd9ed7e61a9dd8479f72160713684ea66d4
aav265 <98988446+aav265@users.noreply.github.com>
2022-02-23 16:45:30 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -74,7 +74,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     if recv7[:3] != '221':
         print('221 reply not received from server.')
     # Fill in end
-
+    
+    clientSocket.close()
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
